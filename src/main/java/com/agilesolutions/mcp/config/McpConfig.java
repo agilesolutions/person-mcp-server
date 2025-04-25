52b4bc57d562a9d0dba748d13a6c93b4b97a09cf
admin <rong@agile-solutions.ch>
2025-04-25 14:28:18 +0200

    fixes

--- src/main/java/com/agilesolutions/mcp/config/McpConfig.java
+++ src/main/java/com/agilesolutions/mcp/config/McpConfig.java
@@ -1,10 +1,14 @@
 package com.agilesolutions.mcp.config;
 
 import com.agilesolutions.mcp.tools.PersonTools;
+import io.modelcontextprotocol.server.McpServerFeatures;
+import io.modelcontextprotocol.spec.McpSchema;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class McpConfig {
@@ -14,5 +18,20 @@ public class McpConfig {
         return MethodToolCallbackProvider.builder()
                 .toolObjects(personTools)
                 .build();
+    }
+
+    @Bean
+    public List<McpServerFeatures.SyncPromptRegistration> prompts() {
+        var prompt = new McpSchema.Prompt("persons-by-nationality", "Get persons by nationality",
+                List.of(new McpSchema.PromptArgument("nationality", "Person nationality", true)));
+
+        var promptRegistration = new McpServerFeatures.SyncPromptRegistration(prompt, getPromptRequest -> {
+            String nameArgument = (String) getPromptRequest.arguments().get("name");
+            var userMessage = new McpSchema.PromptMessage(McpSchema.Role.USER,
+                    new McpSchema.TextContent("How many persons are from " + nameArgument + " ?"));
+            return new McpSchema.GetPromptResult("Count persons by nationality", List.of(userMessage));
+        });
+
+        return List.of(promptRegistration);
     }
 }
